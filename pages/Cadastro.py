d412394565c88d478c354d173121c28e2f1e8b0d
DamarisLopes21 <damarislopeslima63@gmail.com>
2023-11-07 21:38:42 -0400

    first commit

---
+++ pages/Cadastro.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import mysql.connector
+
+def insert_user(nome, senha):
+    conn = mysql.connector.connect(
+        host='localhost',
+        database='nome',
+        user='root',
+        password='DAMARIS21$'
+    )
+
+    cursor = conn.cursor()
+    query = "INSERT INTO cadastro (nome, senha) VALUES (%s, %s)"
+    cursor.execute(query, (nome, senha))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+# Configuração da página de cadastro
+
+st.title("Página de Cadastro")
+
+nome = st.text_input("Nome")
+senha = st.text_input("Senha", type="password")
+
+if st.button("Cadastrar"):
+    insert_user(nome, senha)
+    st.success("Cadastro realizado com sucesso!")
